dce666263532fe42c6600f8ece538c551cdaa065
wrodriguess77 <willrodrigues4@gmail.com>
2022-12-17 14:22:31 -0300

    Aula 06 - Linkando controller a rota

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(User $user)
+    {
+        return $user->all();
+    }
+
+    public function show(User $user)
+    {
+        return $user;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello-world', function () {
-    return '<h1>Hello World</h1>';
-});
-
-Route::get('/users/{id}', function ($id) {
-    return [
-        "user" => "xyz",
-        "id" => $id
-    ];
-});
-
-Route::get('/users/{paramB}/{paramA}', function ($paramA, $paramB) {
-    return [
-        "paramA" => $paramA,
-        "paramB" => $paramB
-    ];
-});
-
-Route::get('/empresa/{nomeDaEmpresa?}', function ($nomaDaEmpresa = null) {
-    return "<h1>A empresa " . ($nomaDaEmpresa ?? "Fantasma") . "</h1>";
-});
-
-Route::prefix('/usuarios')->group(function () {
-    Route::get('/', function () {
-        return 'Rota: /usuarios';
-    })->name('usuarios');
-    Route::get('/findOne', function () {
-        return 'Rota: /usuarios/findOne';
-    })->name('usuarios.findOne');
-});
-
-// Route::get('/user/{user}', function (User $user) {
-//     // dd($user);
-//     return $user;
-// });
-
-// Dizendo ao laravel que irei buscar o usuário utilizando o email
-Route::get('/user/{user:email}', function (User $user) {
-    return $user;
-});
-
-Route::get('/request', function (Request $request) {
-    dd($request->all());
-});
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{user}', [UserController::class, 'show']);
